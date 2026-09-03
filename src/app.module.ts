8a4211b7a79b3794d2c53bc7b85163fae782570e
Nastya <anastasion0107@gmail.com>
2026-09-04 00:44:49 +0700

    init project

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller.js';
+import { AppService } from './app.service.js';
+import { TaskModule } from './task/task.module.js';
+
+@Module({
+  imports: [TaskModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
